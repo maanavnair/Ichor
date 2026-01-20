c50ae629fd9b15b5ba90be889e8d50e9e75c4bf1
Maanav Nair <maanavnair20042@gmail.com>
2026-01-21 00:46:53 +0530

    added sprinting

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -9,7 +9,8 @@ public class Player : MonoBehaviour
     public Animator anim;
 
     [Header("Movement Variables")]
-    public float speed;
+    public float walkSpeed;
+    public float runSpeed = 8;
     public float jumpForce;
     public float jumpCutMultiplier = 0.5f;
     public float normalGravity;
@@ -20,6 +21,7 @@ public class Player : MonoBehaviour
 
     //Inputs
     private Vector2 moveInput;
+    private bool runPressed;
     private bool jumpPressed;
     private bool jumpReleased;
 
@@ -50,7 +52,8 @@ public class Player : MonoBehaviour
 
     private void HandleMovement()
     {
-        float targetSpeed = moveInput.x * speed;
+        float currentSpeed = runPressed ? runSpeed : walkSpeed;
+        float targetSpeed = moveInput.x * currentSpeed;
         rb.linearVelocity = new Vector2(targetSpeed, rb.linearVelocity.y);
     }
 
@@ -100,8 +103,11 @@ public class Player : MonoBehaviour
 
         anim.SetFloat("yVelocity", rb.linearVelocity.y);
 
-        anim.SetBool("isIdle", Mathf.Abs(moveInput.x) < .1f && isGrounded);
-        anim.SetBool("isWalking", Mathf.Abs(moveInput.x) > .1f && isGrounded);
+        bool isMoving = Mathf.Abs(moveInput.x) > .1f && isGrounded;
+
+        anim.SetBool("isIdle", !isMoving && isGrounded);
+        anim.SetBool("isWalking", isMoving && !runPressed);
+        anim.SetBool("isRunning", isMoving && runPressed);
     }
     
     void Flip()
@@ -120,6 +126,11 @@ public class Player : MonoBehaviour
     public void OnMove(InputValue value)
     {
         moveInput = value.Get<Vector2>();
+    }
+
+    public void OnRun(InputValue value)
+    {
+        runPressed = value.isPressed;
     }
 
     public void OnJump(InputValue value)
